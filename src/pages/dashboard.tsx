a548be8e7e569cece9c18e3fa354f5dc34ec3adf
Diego Araújo <brhdn050654@ambev.com.br>
2022-09-21 16:32:06 -0300

    Finalizando página de dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,12 +2,13 @@ import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Sidebar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
 import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts";
 
 const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
-const options = {
+const options: ApexOptions = {
   chart: {
     toolbar: {
       show: false,
@@ -27,11 +28,17 @@ const options = {
     enabled: false,
   },
   xaxis: {
-    type: "datetime",
+    labels: {
+      formatter: function (val) {
+        return new Date(val).toLocaleDateString();
+      },
+    },
     axisBorder: {
       color: theme.colors.gray[600],
     },
-    axisTicks: { color: theme.colors.gray[600] },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
     categories: [
       "2021-03-18T00:00:00.000Z",
       "2021-03-19T00:00:00.000Z",
@@ -41,6 +48,15 @@ const options = {
       "2021-03-23T00:00:00.000Z",
       "2021-03-24T00:00:00.000Z",
     ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
   },
 };
 const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 18, 109] }];
@@ -58,16 +74,17 @@ export default function Dashboard() {
           minChildWidth={320}
           alignContent="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
       </Flex>
