acfd786a852bb7acfd72df7dac041b716e59615b
Diego Araújo <brhdn050654@ambev.com.br>
2022-09-22 10:54:21 -0300

    Implementando tela de users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,32 @@
+import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+import { Sidebar } from "../../components/Form/Sidebar";
+import { Header } from "../../components/Header";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
